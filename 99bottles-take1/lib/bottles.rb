68c00d13f1158390db36283b0788ad26b891136e
Josh Cheek <josh.cheek@gmail.com>
2018-06-07 15:21:57 -0600

    Inline Bottles#first_part and #second_part into #verse

--- 99bottles-take1/lib/bottles.rb
+++ 99bottles-take1/lib/bottles.rb
@@ -11,27 +11,22 @@ class Bottles
   end
 
   def verse(number_of_bottles)
-    first_part(number_of_bottles) + "\n" + second_part(number_of_bottles) + "\n"
+    num_left     = number_of_bottles - 1
+    post_bottles = bottle_str(num_left)
+    pre_bottles  = bottle_str(number_of_bottles)
+    if number_of_bottles == 1
+      "#{number_of_bottles} #{pre_bottles} of beer on the wall, #{number_of_bottles} #{pre_bottles} of beer.\n" +
+        "Take it down and pass it around, no more bottles of beer on the wall.\n"
+    elsif number_of_bottles == 0
+      "No more #{pre_bottles} of beer on the wall, no more #{pre_bottles} of beer.\n" +
+        "Go to the store and buy some more, 99 bottles of beer on the wall.\n"
+    else
+      "#{number_of_bottles} #{pre_bottles} of beer on the wall, #{number_of_bottles} #{pre_bottles} of beer.\n" +
+        "Take one down and pass it around, #{num_left} #{post_bottles} of beer on the wall.\n"
+    end
   end
 
   private
-
-  def first_part(number_of_bottles)
-    pre_bottles  = number_of_bottles.zero? ? "no more" : number_of_bottles.to_s
-    post_bottles = bottle_str(number_of_bottles)
-    "#{pre_bottles.capitalize} #{post_bottles} of beer on the wall, #{pre_bottles} #{post_bottles} of beer."
-  end
-
-  def second_part(number_of_bottles)
-    if number_of_bottles == 1
-      "Take it down and pass it around, no more bottles of beer on the wall."
-    elsif number_of_bottles == 0
-      "Go to the store and buy some more, 99 bottles of beer on the wall."
-    else
-      num_left = number_of_bottles - 1
-      "Take one down and pass it around, #{num_left} #{bottle_str(num_left)} of beer on the wall."
-    end
-  end
 
   def bottle_str(number_of_bottles)
     number_of_bottles == 1 ? 'bottle' : 'bottles'
